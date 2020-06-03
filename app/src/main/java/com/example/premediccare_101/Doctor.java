2af8f5cbc000ef418c092107ae05b18d96d2e0f8
KIIT <asstore3@gmail.com>
2020-06-03 22:44:09 +0530

    Incomplete work of dialogfragment with unversioned files.

---
+++ app/src/main/java/com/example/premediccare_101/Doctor.java
@@ -0,0 +1,16 @@
+package com.example.premediccare_101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor);
+        WebView webview;
+    }
+}
