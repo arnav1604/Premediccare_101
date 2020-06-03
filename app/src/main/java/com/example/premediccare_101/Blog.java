5a1f9d4377268e539596b61603d37f57f16046bb
Arnav <arnav1604.pat@gmail.com>
2020-06-03 18:36:11 +0530

    Blog

---
+++ app/src/main/java/com/example/premediccare_101/Blog.java
@@ -0,0 +1,29 @@
+package com.example.premediccare_101;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Blog extends Fragment {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_heart_disease, container, false);
+        WebView webView = view.findViewById(R.id.blog);
+        WebSettings webSettings=webView.getSettings();//initialising web view object
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://premediccare.rf.gd/blog-user.php");
+        return view;
+    }
+}
--- app/src/main/java/com/example/premediccare_101/HomeFragment.java
+++ app/src/main/java/com/example/premediccare_101/HomeFragment.java
@@ -10,10 +10,11 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class HomeFragment extends Fragment {
     Button b1, b2, b3, b4, b5;
-
+WebView
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -43,12 +44,18 @@ public class HomeFragment extends Fragment {
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s = "http://premediccare.rf.gd/blog-user.php";
-                Uri u = Uri.parse("http://" + s);
-                Intent i = new Intent(Intent.ACTION_VIEW, u);
-                startActivity(i);
+                b2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String s = "http://premediccare.rf.gd/doctors-user.php";
+                        Uri u = Uri.parse("http://" + s);
+                        Intent i = new Intent(Intent.ACTION_VIEW, u);
+                        startActivity(i);
+                    }
+                });
             }
         });
+
         b4 = v.findViewById(R.id.b_about);
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +77,9 @@ public class HomeFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    private FragmentManager getSupportFragmentManager() {
     }
 
 }
